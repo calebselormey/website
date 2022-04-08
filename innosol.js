ba6ccc2796639c6fd1ee1e4e3b38ffd243462bd4
innosoltech <102552112+innosoltech@users.noreply.github.com>
2022-04-08 16:13:01 +0000

    Update innosol.js

--- innosol.js
+++ innosol.js
@@ -1,5 +1,5 @@
 // Auto Typesript control code
-var typed = new Typed(".auto-type", {
+var typed = new Typed(".we-do", {
   strings: ["SOFTWARES", "WEBSITES", "DIGITAL ADVERTISING", "AND MORE..."],
   typeSpeed: 100,
   backSpeed: 100,
@@ -9,7 +9,7 @@ var typed = new Typed(".auto-type", {
 var a;
 function show_hide() {
   if (a == 1) {
-    document.getElementById("auto-type").style.display = "inline";
+    document.getElementById("auto-type").style.display = "block";
     return (a = 0);
   } else {
     document.getElementById("auto-type").style.display = "none";
@@ -72,63 +72,3 @@ function show_hide() {
 // function closeForm() {
 //   document.getElementById("myModal").style.display = "none";
 // }
-
-// Form processing control code
-// window.addEventListener("DOMContentLoaded", function () {
-//   // get the form elements defined in your form HTML above
-
-//   var form = document.getElementById("my-form");
-//   // var button = document.getElementById("my-form-button");
-//   var status = document.getElementById("status");
-
-//   // Success and Error functions for after the form is submitted
-
-//   function success() {
-//     form.reset();
-//     status.classList.add("success");
-//     status.innerHTML = "Submitted. Thanks!";
-
-//   }
-
-//   function error() {
-//     status.classList.add("error");
-//     status.innerHTML = "Oops! There was a problem. Try again.";
-//   }
-
-//   // handle the form submission event
-
-//   form.addEventListener("submit", function (ev) {
-//     ev.preventDefault();
-//     var data = new FormData(form);
-//     ajax(form.method, form.action, data, success, error);
-//   });
-// });
-
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}
-
-// open & close form control code
-// function openForm() {
-//   document.getElementById("myModal").style.display = "block";
-// }
-
-// function closeForm() {
-//   document.getElementById("myModal").style.display = "none";
-// }
-
-// testing
-// var btn = document.getElementById("myBtn");
